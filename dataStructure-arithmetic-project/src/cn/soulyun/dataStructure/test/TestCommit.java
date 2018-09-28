7c3da9e95db31960b422d4e0e17f36e26d51d821
Varmonson <15262400743@163.com>
2018-09-28 10:45:39 +0800

    测试初始版本提交到master分支

---
+++ dataStructure-arithmetic-project/src/cn/soulyun/dataStructure/test/TestCommit.java
@@ -0,0 +1,15 @@
+package cn.soulyun.dataStructure.test;
+
+/**
+ * @program: dataStructure-arithmetic-project
+ * @description: 测试初始版本提交
+ * @author: fhq
+ * @create: 2018-09-28 10:31
+ **/
+public class TestCommit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
